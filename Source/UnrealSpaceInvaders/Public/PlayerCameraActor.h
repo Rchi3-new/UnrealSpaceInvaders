9b2d0c77d92862b40a7475e7fdfc9d529887a7c2
Archie2282 <gtuchkov@me.com>
2024-02-08 21:02:16 +0300

    [ADD] Players Camera

---
+++ Source/UnrealSpaceInvaders/Public/PlayerCameraActor.h
@@ -0,0 +1,17 @@
+// No Copyright today, comrade.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "PlayerCameraActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSPACEINVADERS_API APlayerCameraActor : public ACameraActor
+{
+	GENERATED_BODY()
+	
+};
--- Source/UnrealSpaceInvaders/ShipPlayerController.cpp
+++ Source/UnrealSpaceInvaders/ShipPlayerController.cpp
@@ -2,4 +2,17 @@
 
 
 #include "ShipPlayerController.h"
+#include "Public/PlayerCameraActor.h"
+#include "Kismet/GameplayStatics.h"
 
+void AShipPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	SetInputMode(FInputModeGameOnly();
+	AActor* CameraActor = UGameplayStatics::GetActorOfClass(GetWorld(), APlayerCameraActor::StaticClass());
+	CameraActor = Cast<class APlayerCameraActor>(CameraActor);
+	if(CameraActor)
+	{ 
+		SetViewTargetWithBlend(CameraActor);
+	}
+}
