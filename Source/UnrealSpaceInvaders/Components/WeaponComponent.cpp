f6541e8600ce31d2e3083bda9b762831ea5c9a04
Archie2282 <gtuchkov@me.com>
2025-01-07 08:45:11 +0300

    Weapon component implemented

--- Source/UnrealSpaceInvaders/Components/WeaponComponent.cpp
+++ Source/UnrealSpaceInvaders/Components/WeaponComponent.cpp
@@ -1,8 +1,12 @@
 #include "WeaponComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 UWeaponComponent::UWeaponComponent()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	FireRate = 1.0f; // Один выстрел в секунду
+	ProjectileSpeed = 500.0f; // Скорость снаряда
+	SpawnOffset = FVector(0.0f, 0.0f, 50.0f); // Смещение от центра актора
+	bFireUpwards = true; // По умолчанию стрельба вверх
 }
 
 void UWeaponComponent::BeginPlay()
@@ -10,8 +14,41 @@ void UWeaponComponent::BeginPlay()
 	Super::BeginPlay();
 }
 
-void UWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType,
-                                     FActorComponentTickFunction* ThisTickFunction)
+void UWeaponComponent::StartFiring()
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (!FiringTimerHandle.IsValid())
+	{
+		GetWorld()->GetTimerManager().SetTimer(FiringTimerHandle, this, &UWeaponComponent::Fire, FireRate, true);
+	}
+}
+
+void UWeaponComponent::StopFiring()
+{
+	GetWorld()->GetTimerManager().ClearTimer(FiringTimerHandle);
+}
+
+void UWeaponComponent::Fire()
+{
+	if (!ProjectileClass)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("ProjectileClass is not set for WeaponComponent!"));
+		return;
+	}
+
+	if (AActor* Owner = GetOwner())
+	{
+		FVector Direction = bFireUpwards ? FVector::UpVector : FVector::DownVector;
+		FVector SpawnLocation = Owner->GetActorLocation() + SpawnOffset;
+		AActor* SpawnedProjectile = GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnLocation, Direction.Rotation());
+		if (SpawnedProjectile)
+		{
+			UPrimitiveComponent* RootComponent = Cast<UPrimitiveComponent>(SpawnedProjectile->GetRootComponent());
+			if (RootComponent)
+			{
+				RootComponent->SetPhysicsLinearVelocity(Direction * ProjectileSpeed);
+			}
+		}
+
+		OnFire.Broadcast();
+	}
 }
--- Source/UnrealSpaceInvaders/Components/WeaponComponent.h
+++ Source/UnrealSpaceInvaders/Components/WeaponComponent.h
@@ -4,6 +4,8 @@
 #include "Components/ActorComponent.h"
 #include "WeaponComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnFire);
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class UNREALSPACEINVADERS_API UWeaponComponent : public UActorComponent
 {
@@ -12,10 +14,34 @@ class UNREALSPACEINVADERS_API UWeaponComponent : public UActorComponent
 public:
 	UWeaponComponent();
 
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void StartFiring();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void StopFiring();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	TSubclassOf<AActor> ProjectileClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon", meta = (ClampMin = "0.1"))
+	float FireRate;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	FVector SpawnOffset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	float ProjectileSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	bool bFireUpwards; // true = игрок стреляет вверх, false = враги стреляют вниз
+
+	UPROPERTY(BlueprintAssignable, Category = "Events")
+	FOnFire OnFire;
+
 protected:
 	virtual void BeginPlay() override;
 
-public:
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	FTimerHandle FiringTimerHandle;
+	void Fire();
 };
